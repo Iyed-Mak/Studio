31602592b11d6f1daa0fad0d9c5b189519a39625
Iyed Makouf <iyed.makouf@univ-constantine2.dz>
2024-12-07 11:45:40 +0100

    Studio website

---
+++ Js/script.js
@@ -0,0 +1,14 @@
+// start navigation
+window.onscroll=()=>{
+  document.querySelector(".navbar").style.background="var(--navColor)";
+  document.querySelector(".navbar").classList.remove("p-4");
+  document.querySelector(".navbar").classList.add("p-3");
+  document.querySelector(".navbar-brand").style.width = "10rem"; 
+}
+
+
+
+
+
+// end navigation
+
